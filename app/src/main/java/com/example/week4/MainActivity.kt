b2fc882383fa4a5b4556ce92829f34573a1cdd31
Yueying Ke <yxk200018@utdallas.com>
2023-03-24 21:07:03 -0500

    version2.0 Appointment maker

--- app/src/main/java/com/example/week4/MainActivity.kt
+++ app/src/main/java/com/example/week4/MainActivity.kt
@@ -1,34 +1,46 @@
 package com.example.week4
 
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    //var billAmount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         // my code starts here
         //create a reference to the button by using the findViewById() method and assigning it to a variable
-        val calculateButton = findViewById<Button>(R.id.calculate)
+        val confirmButton = findViewById<Button>(R.id.confirm)
         // same with others, create reference
-        val editAmount = findViewById<EditText>(R.id.enterBill)
-        val totalDisplay = findViewById<TextView>(R.id.total)
+        val resultDisplay = findViewById<TextView>(R.id.result)
+        val datePicker = findViewById<DatePicker>(R.id.datePicker)
+
+        // Obtain the current date using the Calendar class
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Set the current date on the DatePicker
+
+        datePicker.init(year, month, day
+        ) { view, year, monthOfYear, dayOfMonth -> // Update the TextView with the selected date
+            val dateString = "$dayOfMonth/${monthOfYear + 1}/$year"
+            resultDisplay.text = dateString
+        }
+
         /*Set a click listener on the button by calling the setOnClickListener() method on the button variable and passing in a lambda function that will be called when the button is clicked*/
-        calculateButton.setOnClickListener {
-            val editAmount = editAmount.text.toString().toFloatOrNull()
-            // Check if the input is valid
-            if (editAmount == null) {
-                totalDisplay.text = "Invalid input"
-            } else {
-                // Calculate the tax amount and display it in the TextView
-                val totalAmount = editAmount*1.0625
-                totalDisplay.text = "Your total is: $totalAmount"
-            }
+        confirmButton.setOnClickListener {
+            val dateString = "${datePicker.dayOfMonth}/${datePicker.month+1}/${datePicker.year}"
+            val nameEntered = findViewById<EditText>(R.id.username).text.toString()
+            val message = getString(R.string.appointment_message, nameEntered, dateString)
+            resultDisplay.text = message
         }
     }
 }
